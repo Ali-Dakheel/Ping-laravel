cf82a76f138863749d34b86c1c1212e0f382757a
Ali-dakheel <alidakheel819@gmail.com>
2025-12-06 15:03:40 +0300

    cleanup

--- routes/api/routes.php
+++ routes/api/routes.php
@@ -1,8 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
+declare(strict_types=1);
+
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
