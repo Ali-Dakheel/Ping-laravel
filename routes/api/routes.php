c09edceb5c38e394778bee2e87702bb2bb84d1a3
Ali-dakheel <alidakheel819@gmail.com>
2025-12-13 10:24:27 +0300

    add rate limiting service provider

--- routes/api/routes.php
+++ routes/api/routes.php
@@ -5,12 +5,12 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
-Route::middleware(['auth:sanctum'])->group(static function (): void {
+Route::middleware(['auth:sanctum', 'throttle:api'])->group(static function (): void {
     Route::get('user', static function (Request $request) {
         return $request->user()->name('user');
     });
 
-    Route::prefix('services')->as('services:')->group(base_path('routes/api/services.php'));
+    Route::prefix('services')->as('services:')->group(base_path('routes/api/services.php'))->middleware(['throttle:100,1']);
     Route::prefix('checks')->as('checks:')->group(base_path('routes/api/checks.php'));
     Route::prefix('credentials')->as('credentials:')->group(base_path('routes/api/credentials.php'));
 
